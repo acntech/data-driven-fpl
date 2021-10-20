0d34b186a38fcb9b2937e3104c44c39063a01f59
Martin Gran <martgra@gmail.com>
2021-10-20 08:32:16 +0000

    add startpoint for second session

--- fpl/cli.py
+++ fpl/cli.py
@@ -28,3 +28,11 @@ def data():
 def download(container):
     """Download all blobs."""
     download_all(STORAGE_ACCOUNT, container)
+
+
+@data.command(name="to-csv", help="Convert JSON to CSV")
+def to_csv():
+    """Convert to CSV."""
+    # TODO: Add entry to start convertion of json to CSV from command line
+    # CLI must support change of source directory and the entities "teams" and "elements"
+    print("FILL INN LOGIC!")
