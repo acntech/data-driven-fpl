baf5c4602f6fcbb85660e3fe5168a7e93628986b
Martin Gran <martgra@gmail.com>
2021-12-02 13:41:02 +0000

    fix bug in to_parquet

--- fpl/data/make_parquet.py
+++ fpl/data/make_parquet.py
@@ -15,9 +15,12 @@ def to_parquet(path: str, output_path: str, chunk_size=300000, partition_cols=No
         chunk_size (int, optional): Number of CSV lines to read at the time. Defaults to 300000.
         partition_cols (list, optional): List of columns to partition dataset on. Defaults to None.
     """
-    num_lines = sum([1 for i in open(path)]) // chunk_size + 1
+    num_lines = sum([1 for i in open(path)])
+    n_chunks = (
+        num_lines // chunk_size if num_lines % chunk_size == 0 else num_lines // chunk_size + 1
+    )
 
-    for y, chunk in enumerate(tqdm(pd.read_csv(path, chunksize=chunk_size), total=num_lines)):
+    for y, chunk in enumerate(tqdm(pd.read_csv(path, chunksize=chunk_size), total=n_chunks)):
         table = pa.Table.from_pandas(chunk)
         ds.write_dataset(
             table,
@@ -32,7 +35,7 @@ def to_parquet(path: str, output_path: str, chunk_size=300000, partition_cols=No
 
 if __name__ == "__main__":
     to_parquet(
-        "data/interim/2021-fpl-data_elements.csv",
-        "data/interim/2021_elements_parquet",
+        "data/interim/2021-fpl-data_teams.csv",
+        "data/interim/2021_teams_parquet",
         partition_cols=["team_code", "code"],
     )
