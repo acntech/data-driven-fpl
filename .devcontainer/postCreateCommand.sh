9e0813843ba64268f0cdc26731c5eb3a894fcfa0
Martin Gran <martgra@gmail.com>
2021-12-03 09:21:12 +0100

    Add fix to make poetry install robust (#17)
    * Add fix to make poetry install robust
    
    * add pre-commit install to postcommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+
 poetry config virtualenvs.create false
 poetry env use system
+
+# Workarount to try to mitigate SSL verification issue.
+# Poetry SSL issue: https://stackoverflow.com/questions/48391750/disable-python-requests-ssl-validation-for-an-imported-module
+
 poetry install
+ret=$?
+if [ $ret -ne 0 ]; then
+        export  CURL_CA_BUNDLE=""
+        poetry install
+else
+        echo "Install sucess!!"
+fi
+
 
 jupyter notebook --generate-config
 echo 'c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"' >> /home/vscode/.jupyter/jupyter_notebook_config.py
 which jupyter
 jupyter nbextension install --py jupytext --user
 jupyter nbextension enable jupytext --user --py
+pre-commit install
